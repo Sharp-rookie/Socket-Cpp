47679fa84bab5db36c5603a85bd83ebc4e34c02d
Sharp-rookie <1842604700@qq.com>
2021-08-26 21:07:00 +0800

    2021-8-26

--- Linux_cpp/Multi-Progress/getpid.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <sys/types.h>
-#include <unistd.h>
-
-int main()
-{
-    printf("本程序的进程编号是：%d\n", getpid());
-    sleep(30); // 是为了方便查看进程在shell下用ps -ef|grep book251查看本进程的编号。
-}
